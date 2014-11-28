32dab0ea6ce353fb7c7a82dd8e77fef65a746fcb
chrisharden1 <chris@chrisharden.com>
2014-11-28 14:11:02 -0500

    moved echo.js to root of Echo folder in attempt to address path join issue that cordova plugin add fails on

--- www/echo.js
+++
@@ -1,9 +0,0 @@
-//Harden
-/**
- * Hack attempt at creating javascript interface for Echo plugin
- */
-window.echo = function(str, callback) {
-    cordova.exec(callback, function(err) {
-                 callback('Nothing to echo.');
-                 }, "Echo", "echo", [str]);
-};
